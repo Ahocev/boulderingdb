547ec9f74d65adf6f7dac551ee02325f1612fceb
Ahocev <Ahocev@gmail.com>
2024-07-31 11:20:48 -0400

    autowired AuthenticationManager into AuthenticatedUserUtilities in order to get manualAuthentication function to work

--- src/main/java/info/alexhocevarsmith/boulderingdb/security/AuthenticatedUserUtilities.java
+++ src/main/java/info/alexhocevarsmith/boulderingdb/security/AuthenticatedUserUtilities.java
@@ -2,11 +2,16 @@ package info.alexhocevarsmith.boulderingdb.security;
 
 import info.alexhocevarsmith.boulderingdb.database.dao.UserDAO;
 import info.alexhocevarsmith.boulderingdb.database.entity.User;
+import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.stereotype.Component;
 
 @Slf4j
@@ -15,6 +20,9 @@ public class AuthenticatedUserUtilities {
 
     @Autowired
     private UserDAO userDAO;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
 
     public String getCurrentUsername() {
         // the goal of this method is to either return the logged in username or null if the user is not logged in
@@ -41,4 +49,14 @@ public class AuthenticatedUserUtilities {
         }
         return userDAO.findByEmailIgnoreCase(username);
     }
+
+    public void manualAuthentication(HttpSession session, String username, String unencryptedPassword) {
+        // reset security principal to be the new user information
+        Authentication request = new UsernamePasswordAuthenticationToken(username, unencryptedPassword);
+        Authentication result = authenticationManager.authenticate(request);
+        SecurityContext sc = SecurityContextHolder.getContext();
+        sc.setAuthentication(result);
+        session.setAttribute(HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY, sc);
+    }
+
 }
