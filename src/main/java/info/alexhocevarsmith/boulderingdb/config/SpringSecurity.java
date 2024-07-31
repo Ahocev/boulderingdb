fd062dc27b4552e228d88ea3b4d3c4bd6b68a2e3
Ahocev <Ahocev@gmail.com>
2024-07-31 11:19:06 -0400

    added AuthenticationManager bean to SpringSecurity config

--- src/main/java/info/alexhocevarsmith/boulderingdb/config/SpringSecurity.java
+++ src/main/java/info/alexhocevarsmith/boulderingdb/config/SpringSecurity.java
@@ -2,6 +2,8 @@ package info.alexhocevarsmith.boulderingdb.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -40,4 +42,10 @@ public class SpringSecurity {
     public PasswordEncoder getPasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
+    }
+
 }
