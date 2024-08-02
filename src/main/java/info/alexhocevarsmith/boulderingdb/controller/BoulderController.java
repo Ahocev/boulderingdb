8aa608ad27926595b4d8a042a18d065a94bb4268
Ahocev <Ahocev@gmail.com>
2024-08-02 15:21:44 -0400

    added a search page to the boulder controller

--- src/main/java/info/alexhocevarsmith/boulderingdb/controller/BoulderController.java
+++ src/main/java/info/alexhocevarsmith/boulderingdb/controller/BoulderController.java
@@ -1,15 +1,27 @@
 package info.alexhocevarsmith.boulderingdb.controller;
 
+import info.alexhocevarsmith.boulderingdb.form.RegisterAccountFormBean;
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Slf4j
 @Controller
 @RequestMapping("/boulder")
 public class BoulderController {
+
+//    @Autowired
+//    private BoulderDAO boulderDao;
 
     @GetMapping("/boulder-page")
     public ModelAndView boulderPage() {
@@ -23,6 +35,29 @@ public class BoulderController {
 
         return new ModelAndView("boulder/browse");
 
+    }
+
+    @GetMapping("/search")
+    public ModelAndView search(@RequestParam(required = false) String search) {
+        // this page is for another page of the website which is express as "/page-url"
+        ModelAndView response = new ModelAndView("boulder/search");
+
+        log.debug("The user searched for the term: " + search);
+
+
+        response.addObject("search", search);
+
+//        List<Boulder> products = boulderDao.findById(search);
+//        response.addObject("boulders", boulders);
+
+        return response;
+    }
+
+    @RequestMapping(value = "/boulder-input", method = { RequestMethod.POST, RequestMethod.GET })
+    public ModelAndView boulderInput(@Valid RegisterAccountFormBean form, BindingResult bindingResult, HttpSession session) {
+        ModelAndView response = new ModelAndView("boulder/boulder-input");
+
+        return response;
     }
 
 }
