11b8f7d11a338b0d81ebbdb899507709ffe06e4c
Ahocev <Ahocev@gmail.com>
2024-08-03 13:49:06 -0400

    setup boulder controller to handle boulder-input submit

--- src/main/java/info/alexhocevarsmith/boulderingdb/controller/BoulderController.java
+++ src/main/java/info/alexhocevarsmith/boulderingdb/controller/BoulderController.java
@@ -1,16 +1,19 @@
 package info.alexhocevarsmith.boulderingdb.controller;
 
+import info.alexhocevarsmith.boulderingdb.database.dao.BoulderProblemDAO;
+import info.alexhocevarsmith.boulderingdb.database.entity.BoulderProblem;
+import info.alexhocevarsmith.boulderingdb.form.AddBoulderFormBean;
 import info.alexhocevarsmith.boulderingdb.form.RegisterAccountFormBean;
+import info.alexhocevarsmith.boulderingdb.service.BoulderService;
 import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
@@ -20,8 +23,11 @@ import java.util.List;
 @RequestMapping("/boulder")
 public class BoulderController {
 
-//    @Autowired
-//    private BoulderDAO boulderDao;
+    @Autowired
+    private BoulderProblemDAO boulderProblemDAO;
+
+    @Autowired
+    private BoulderService boulderService;
 
     @GetMapping("/boulder-page")
     public ModelAndView boulderPage() {
@@ -47,17 +53,44 @@ public class BoulderController {
 
         response.addObject("search", search);
 
-//        List<Boulder> products = boulderDao.findById(search);
-//        response.addObject("boulders", boulders);
+//        List<BoulderProblem> boulderProblems = boulderProblemDAO.findById(search);
+//        response.addObject("boulderProblems", boulderProblems);
 
         return response;
     }
 
-    @RequestMapping(value = "/boulder-input", method = { RequestMethod.POST, RequestMethod.GET })
-    public ModelAndView boulderInput(@Valid RegisterAccountFormBean form, BindingResult bindingResult, HttpSession session) {
+    @GetMapping("/boulder-input")
+    public ModelAndView boulderInput() {
         ModelAndView response = new ModelAndView("boulder/boulder-input");
-
+        response.addObject("form", new AddBoulderFormBean());
         return response;
+    }
+
+    @PostMapping("/submit")
+    public ModelAndView submitBoulderProblem(@Valid AddBoulderFormBean form, BindingResult bindingResult) {
+        ModelAndView response = new ModelAndView();
+
+        // Check if the BoulderProblem name already exists
+        BoulderProblem existingBoulderProblem = boulderProblemDAO.findByBoulderProblemNameIgnoreCase(form.getBoulderProblemName());
+        if (existingBoulderProblem != null) {
+            bindingResult.rejectValue("boulderProblemName", "boulderProblemName", "This Boulder Problem name already exists.");
+        }
+
+        if (bindingResult.hasErrors()) {
+            for (ObjectError error : bindingResult.getAllErrors()) {
+                log.debug("Validation error : " + ((FieldError) error).getField() + " = " + error.getDefaultMessage());
+            }
+
+            response.addObject("bindingResult", bindingResult);
+            response.setViewName("boulder/boulder-input");
+            response.addObject("form", form);
+            return response;
+        }
+
+         BoulderProblem boulderProblem = boulderService.addBoulderProblem(form);
+         response.setViewName("redirect:/boulder/boulder-page?id=" + boulderProblem.getId());
+        return response;
+
     }
 
 }
