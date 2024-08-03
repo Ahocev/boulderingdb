045c87043c89e40b2bc7f068a60bb81e27b158f6
Ahocev <Ahocev@gmail.com>
2024-08-03 12:11:08 -0400

    added BoulderService for boulder-input.jsp function

---
+++ src/main/java/info/alexhocevarsmith/boulderingdb/service/BoulderService.java
@@ -0,0 +1,52 @@
+package info.alexhocevarsmith.boulderingdb.service;
+
+import info.alexhocevarsmith.boulderingdb.database.dao.BoulderProblemDAO;
+import info.alexhocevarsmith.boulderingdb.database.dao.LocationDAO;
+import info.alexhocevarsmith.boulderingdb.database.entity.BoulderProblem;
+import info.alexhocevarsmith.boulderingdb.database.entity.Location;
+import info.alexhocevarsmith.boulderingdb.form.AddBoulderFormBean;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class BoulderService {
+
+    @Autowired
+    private LocationDAO locationDAO;
+
+    @Autowired
+    private BoulderProblemDAO boulderProblemDAO;
+
+    public void addBoulderProblem(AddBoulderFormBean form) {
+
+        // Check if the location already exists
+        Location location = locationDAO.findByCountryAndStateAndClimbingArea(form.getCountry(), form.getState(), form.getClimbingArea());
+
+        if (location == null) {
+            // Create a new location if it doesn't exist
+            location = new Location();
+            location.setCountry(form.getCountry());
+            location.setState(form.getState());
+            location.setClimbingArea(form.getClimbingArea());
+            locationDAO.save(location);
+        }
+
+        // Create a new BoulderProblem and set its fields
+        BoulderProblem boulderProblem = new BoulderProblem();
+        boulderProblem.setBoulderProblemName(form.getBoulderProblemName());
+        boulderProblem.setFirstAscensionist(form.getFirstAscensionist());
+        boulderProblem.setGrade(form.getGrade());
+        boulderProblem.setRating(form.getRating());
+        boulderProblem.setRepeated(form.getRepeated());
+        boulderProblem.setHistory(form.getHistory());
+        boulderProblem.setShowcaseImgUrl(form.getShowcaseImgUrl());
+        boulderProblem.setZoneName(form.getZoneName());
+        boulderProblem.setBoulderName(form.getBoulderName());
+        boulderProblem.setLocation(location);
+
+        // Save the BoulderProblem
+        boulderProblemDAO.save(boulderProblem);
+    }
+}
