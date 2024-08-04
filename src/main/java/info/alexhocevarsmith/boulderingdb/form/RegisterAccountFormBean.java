489527b97eb47b7e4b670f53157008438e421a73
Ahocev <Ahocev@gmail.com>
2024-08-04 12:25:45 -0400

    updated RegisterAccountFormBean to include all table columns

--- src/main/java/info/alexhocevarsmith/boulderingdb/form/RegisterAccountFormBean.java
+++ src/main/java/info/alexhocevarsmith/boulderingdb/form/RegisterAccountFormBean.java
@@ -1,7 +1,6 @@
 package info.alexhocevarsmith.boulderingdb.form;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.*;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.validator.constraints.Length;
@@ -12,12 +11,42 @@ public class RegisterAccountFormBean {
 
     private Integer userId;
 
-    @Length(max = 100, message="Email must be less than 100 characters.")
+    @Length(max = 150, message = "Email must be less than 150 characters.")
     @NotEmpty(message = "Email is required.")
     @Email(message = "Must be a valid email.")
     private String email;
 
-    @NotEmpty(message = "Must not be empty.")
+    @Length(max = 150, message = "Password must be less than 150 characters.")
+    @NotEmpty(message = "Password is required.")
     private String password;
 
+    @Length(max = 200, message = "Name must be less than 200 characters.")
+    private String name;
+
+    @Min(value = 0, message = "Age must be a positive integer.")
+    private Integer age;
+
+    @Length(max = 50, message = "Ape index must be less than 50 characters.")
+    private String apeIndex;
+
+    @Min(value = 0, message = "Height must be a positive number.")
+    private Float height;
+
+    @Length(max = 100, message = "Gender must be less than 100 characters.")
+    private String gender;
+
+    @Length(max = 2000, message = "About section must be less than 2000 characters.")
+    private String about;
+
+    @Length(max = 255, message = "Profile image URL must be less than 255 characters.")
+    private String profileImgUrl;
+
+    @Length(max = 255, message = "Style must be less than 255 characters.")
+    private String style;
+
+    @Length(max = 500, message = "Favorite area must be less than 500 characters.")
+    private String favoriteArea;
+
+    @Length(max = 500, message = "Favorite boulder problem must be less than 500 characters.")
+    private String favoriteBoulderProblem;
 }
