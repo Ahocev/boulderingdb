daf34b31e05a0d7cb1bcb8c5ca9224725034516f
Ahocev <Ahocev@gmail.com>
2024-08-04 12:25:20 -0400

    updated user entity to include all table columns

--- src/main/java/info/alexhocevarsmith/boulderingdb/database/entity/User.java
+++ src/main/java/info/alexhocevarsmith/boulderingdb/database/entity/User.java
@@ -21,17 +21,46 @@ public class User {
     @Column(name = "id")
     private Integer id;
 
-    @Column(name = "email")
+    @Column(name = "email", nullable = false, length = 150)
     private String email;
 
-    @Column(name = "password")
+    @Column(name = "password", nullable = false, length = 150)
     private String password;
 
-    @Column(name = "create_date")
+    @Column(name = "create_date", nullable = false)
     @Temporal(TemporalType.TIMESTAMP)
-    private Date createDate;
+    private Date createDate = new Date();
+
+    @Column(name = "name", length = 200)
+    private String name;
+
+    @Column(name = "age")
+    private Integer age;
+
+    @Column(name = "ape_index", length = 50)
+    private String apeIndex;
+
+    @Column(name = "height")
+    private Float height;
+
+    @Column(name = "gender", length = 100)
+    private String gender;
+
+    @Column(name = "about", length = 2000)
+    private String about;
+
+    @Column(name = "profile_img_url")
+    private String profileImgUrl;
+
+    @Column(name = "style")
+    private String style;
+
+    @Column(name = "favorite_area")
+    private String favoriteArea;
+
+    @Column(name = "favorite_boulder_problem")
+    private String favoriteBoulderProblem;
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
     private List<Comment> comments = new ArrayList<>();
-
 }
