543a10b5bf5ca9fd522b3866e19aabdaf051599d
Ahocev <Ahocev@gmail.com>
2024-08-05 13:20:09 -0400

    added list findAll function to user DAO to facilitate search page

--- src/main/java/info/alexhocevarsmith/boulderingdb/database/dao/UserDAO.java
+++ src/main/java/info/alexhocevarsmith/boulderingdb/database/dao/UserDAO.java
@@ -3,10 +3,14 @@ package info.alexhocevarsmith.boulderingdb.database.dao;
 import info.alexhocevarsmith.boulderingdb.database.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserDAO extends JpaRepository<User, Long> {
 
     User findById(Integer id);
 
     User findByEmailIgnoreCase(String email);
+
+    List<User> findAllByNameContainingIgnoreCase(String name);
 
 }
