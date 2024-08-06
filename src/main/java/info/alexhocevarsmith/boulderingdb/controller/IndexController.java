f8b84dfd053979c38fad77787f4df613a673e1a9
Ahocev <Ahocev@gmail.com>
2024-08-06 19:26:53 -0400

    fixed the index controller to load featuredBoulder() into the model

--- src/main/java/info/alexhocevarsmith/boulderingdb/controller/IndexController.java
+++ src/main/java/info/alexhocevarsmith/boulderingdb/controller/IndexController.java
@@ -1,22 +1,41 @@
 package info.alexhocevarsmith.boulderingdb.controller;
 
+import info.alexhocevarsmith.boulderingdb.database.dao.BoulderProblemDAO;
+import info.alexhocevarsmith.boulderingdb.database.entity.BoulderProblem;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Slf4j
 @Controller
 public class IndexController {
 
+    @Autowired
+    private BoulderProblemDAO boulderProblemDAO;
+
     @GetMapping("/")
     public ModelAndView index() {
-        return new ModelAndView("index");
+
+        ModelAndView response = new ModelAndView("index");
+
+        List<BoulderProblem> featuredBoulders = boulderProblemDAO.findRandomBoulders();
+        response.addObject("featuredBoulders", featuredBoulders);
+
+        return response;
     }
 
     @GetMapping("/index")
     public ModelAndView indexPage() {
-        return new ModelAndView("index");
+        ModelAndView response = new ModelAndView("index");
+
+        List<BoulderProblem> featuredBoulders = boulderProblemDAO.findRandomBoulders();
+        response.addObject("featuredBoulders", featuredBoulders);
+
+        return response;
     }
 
 }
