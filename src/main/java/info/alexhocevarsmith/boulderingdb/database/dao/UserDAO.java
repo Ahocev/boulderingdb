fb44ce94d2a6c88e79c32995588510f369117df8
Ahocev <Ahocev@gmail.com>
2024-08-06 10:33:39 -0400

    random import

--- src/main/java/info/alexhocevarsmith/boulderingdb/database/dao/UserDAO.java
+++ src/main/java/info/alexhocevarsmith/boulderingdb/database/dao/UserDAO.java
@@ -4,6 +4,7 @@ import info.alexhocevarsmith.boulderingdb.database.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UserDAO extends JpaRepository<User, Long> {
 
