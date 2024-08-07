8e9e46eae3a602762f49e488826b9f2b074569f2
Ahocev <Ahocev@gmail.com>
2024-08-07 11:12:21 -0400

    Had to add function that deletes all comments by boulderProblemId before attempting to delete boulderProblem by id due to foreign key constraint. Still not working

--- src/main/java/info/alexhocevarsmith/boulderingdb/controller/AdminController.java
+++ src/main/java/info/alexhocevarsmith/boulderingdb/controller/AdminController.java
@@ -1,10 +1,13 @@
 package info.alexhocevarsmith.boulderingdb.controller;
 
 import info.alexhocevarsmith.boulderingdb.database.dao.BoulderProblemDAO;
+import info.alexhocevarsmith.boulderingdb.database.dao.CommentDAO;
 import info.alexhocevarsmith.boulderingdb.database.entity.BoulderProblem;
+import info.alexhocevarsmith.boulderingdb.database.entity.Comment;
 import info.alexhocevarsmith.boulderingdb.database.entity.User;
 import info.alexhocevarsmith.boulderingdb.security.AuthenticatedUserUtilities;
 import lombok.extern.slf4j.Slf4j;
+import org.hibernate.annotations.Comments;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
@@ -29,6 +32,9 @@ public class AdminController {
     @Autowired
     private BoulderProblemDAO boulderProblemDAO;
 
+    @Autowired
+    private CommentDAO commentDAO;
+
     @GetMapping("/dashboard")
     public ModelAndView dashboard() {
         ModelAndView response = new ModelAndView("admin/dashboard");
@@ -42,11 +48,21 @@ public class AdminController {
         return response;
     }
 
-    @Transactional
     @PostMapping("/deleteBoulder")
-    public ModelAndView deleteBoulder(@RequestParam(required = false) Integer id) {
+    public ModelAndView deleteBoulder(@RequestParam Integer id) {
         ModelAndView response = new ModelAndView();
-        boulderProblemDAO.deleteById(id);
+
+        List<Comment> comments = commentDAO.findByBoulderProblemId(id);
+        if (comments != null) {
+            for (Comment comment : comments) {
+                commentDAO.delete(comment);
+            }
+        }
+
+        BoulderProblem boulderProblem = boulderProblemDAO.findById(id);
+        if (boulderProblem != null ) {
+            boulderProblemDAO.delete(boulderProblem);
+        }
         response.setViewName("redirect:/admin/dashboard");
         return response;
     }
