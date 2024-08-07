9907ee506b92687aec9e42656acfad5c18da83f5
Ahocev <Ahocev@gmail.com>
2024-08-07 13:24:06 -0400

    wrote out the JPA query for findByBoulderProblemId to troubleshoot an issue, keeping it for capstone requirements

--- src/main/java/info/alexhocevarsmith/boulderingdb/database/dao/CommentDAO.java
+++ src/main/java/info/alexhocevarsmith/boulderingdb/database/dao/CommentDAO.java
@@ -2,6 +2,7 @@ package info.alexhocevarsmith.boulderingdb.database.dao;
 
 import info.alexhocevarsmith.boulderingdb.database.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -9,6 +10,7 @@ public interface CommentDAO extends JpaRepository<Comment, Long> {
 
     List<Comment> findByUserId(Integer userId);
 
+    @Query("select c from Comment c where c.boulderProblemId = :boulderProblemId")
     List<Comment> findByBoulderProblemId(Integer boulderProblemId);
 
     Comment findById(Integer id);
