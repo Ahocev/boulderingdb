cf5441c1327e96ee8ba21850257b7b58d911ca43
Ahocev <Ahocev@gmail.com>
2024-08-06 22:31:41 -0400

    trying to get delete function to work in the admin controller

--- src/main/java/info/alexhocevarsmith/boulderingdb/controller/AdminController.java
+++ src/main/java/info/alexhocevarsmith/boulderingdb/controller/AdminController.java
@@ -1,14 +1,21 @@
 package info.alexhocevarsmith.boulderingdb.controller;
 
+import info.alexhocevarsmith.boulderingdb.database.dao.BoulderProblemDAO;
+import info.alexhocevarsmith.boulderingdb.database.entity.BoulderProblem;
 import info.alexhocevarsmith.boulderingdb.database.entity.User;
 import info.alexhocevarsmith.boulderingdb.security.AuthenticatedUserUtilities;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Slf4j
 @Controller
@@ -19,6 +26,9 @@ public class AdminController {
     @Autowired
     private AuthenticatedUserUtilities authenticatedUserUtilities;
 
+    @Autowired
+    private BoulderProblemDAO boulderProblemDAO;
+
     @GetMapping("/dashboard")
     public ModelAndView dashboard() {
         ModelAndView response = new ModelAndView("admin/dashboard");
@@ -26,6 +36,19 @@ public class AdminController {
         User user = authenticatedUserUtilities.getCurrentUser();
         log.debug(user.toString());
 
+        List<BoulderProblem> boulderProblems = boulderProblemDAO.findAll();
+        response.addObject("boulderProblems", boulderProblems);
+
+        return response;
+    }
+
+    @Transactional
+    @PostMapping("/deleteBoulder")
+    public ModelAndView deleteBoulder(@RequestParam(required = false) Integer id) {
+        ModelAndView response = new ModelAndView();
+        boulderProblemDAO.deleteById(id);
+        response.setViewName("redirect:/admin/dashboard");
         return response;
     }
 }
+
