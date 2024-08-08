0a8b5d6c09b7353fb312b433e0956a7d0ec8dd1f
Ahocev <Ahocev@gmail.com>
2024-08-08 10:37:22 -0400

    added controller methods to delete locations and users, added a list of locations and users to the admin dashboard model and view

--- src/main/java/info/alexhocevarsmith/boulderingdb/controller/AdminController.java
+++ src/main/java/info/alexhocevarsmith/boulderingdb/controller/AdminController.java
@@ -2,8 +2,11 @@ package info.alexhocevarsmith.boulderingdb.controller;
 
 import info.alexhocevarsmith.boulderingdb.database.dao.BoulderProblemDAO;
 import info.alexhocevarsmith.boulderingdb.database.dao.CommentDAO;
+import info.alexhocevarsmith.boulderingdb.database.dao.LocationDAO;
+import info.alexhocevarsmith.boulderingdb.database.dao.UserDAO;
 import info.alexhocevarsmith.boulderingdb.database.entity.BoulderProblem;
 import info.alexhocevarsmith.boulderingdb.database.entity.Comment;
+import info.alexhocevarsmith.boulderingdb.database.entity.Location;
 import info.alexhocevarsmith.boulderingdb.database.entity.User;
 import info.alexhocevarsmith.boulderingdb.security.AuthenticatedUserUtilities;
 import lombok.extern.slf4j.Slf4j;
@@ -35,6 +38,12 @@ public class AdminController {
     @Autowired
     private CommentDAO commentDAO;
 
+    @Autowired
+    private LocationDAO locationDAO;
+
+    @Autowired
+    private UserDAO userDAO;
+
     @GetMapping("/dashboard")
     public ModelAndView dashboard() {
         ModelAndView response = new ModelAndView("admin/dashboard");
@@ -44,6 +53,12 @@ public class AdminController {
 
         List<BoulderProblem> boulderProblems = boulderProblemDAO.findAll();
         response.addObject("boulderProblems", boulderProblems);
+
+        List<Location> locations = locationDAO.findAll();
+        response.addObject("locations", locations);
+
+        List<User> users = userDAO.findAll();
+        response.addObject("users", users);
 
         return response;
     }
@@ -62,6 +77,30 @@ public class AdminController {
         BoulderProblem boulderProblem = boulderProblemDAO.findById(id);
         if (boulderProblem != null ) {
             boulderProblemDAO.delete(boulderProblem);
+        }
+        response.setViewName("redirect:/admin/dashboard");
+        return response;
+    }
+
+    @PostMapping("/deleteLocation")
+    public ModelAndView deleteLocation(@RequestParam Integer id) {
+        ModelAndView response = new ModelAndView();
+
+        Location location = locationDAO.findById(id);
+        if (location != null ) {
+            locationDAO.delete(location);
+        }
+        response.setViewName("redirect:/admin/dashboard");
+        return response;
+    }
+
+    @PostMapping("/deleteUser")
+    public ModelAndView deleteUser(@RequestParam Integer id) {
+        ModelAndView response = new ModelAndView();
+
+        User user = userDAO.findById(id);
+        if (user != null ) {
+            userDAO.delete(user);
         }
         response.setViewName("redirect:/admin/dashboard");
         return response;
