6b7144493cce837b547bae68ec2cf1917e65756c
Ahocev <Ahocev@gmail.com>
2024-08-20 12:04:25 -0400

    added findByImageUrl to AdditionalImageDAO to be able to check for duplicate images in the database before uploading

--- src/main/java/info/alexhocevarsmith/boulderingdb/database/dao/AdditionalImageDAO.java
+++ src/main/java/info/alexhocevarsmith/boulderingdb/database/dao/AdditionalImageDAO.java
@@ -1,2 +1,12 @@
-package info.alexhocevarsmith.boulderingdb.database.dao;public interface AdditionalImageDAO {
+package info.alexhocevarsmith.boulderingdb.database.dao;
+
+import info.alexhocevarsmith.boulderingdb.database.entity.AdditionalImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AdditionalImageDAO extends JpaRepository<AdditionalImage, Long> {
+
+    AdditionalImage findById(Integer id);
+
+    AdditionalImage findByImageUrl(String imageUrl);
+
 }
