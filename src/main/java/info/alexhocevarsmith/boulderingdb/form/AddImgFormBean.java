dd8c2b5d486c391eea32783970ab5f15d45fb251
Ahocev <Ahocev@gmail.com>
2024-08-20 09:17:00 -0700

    created AddImgFormBean to accomodate upload of additional images

--- src/main/java/info/alexhocevarsmith/boulderingdb/form/AddImgFormBean.java
+++ src/main/java/info/alexhocevarsmith/boulderingdb/form/AddImgFormBean.java
@@ -1,2 +1,29 @@
-package info.alexhocevarsmith.boulderingdb.form;public class AddImgFormBean {
+package info.alexhocevarsmith.boulderingdb.form;
+
+import info.alexhocevarsmith.boulderingdb.database.entity.BoulderProblem;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@ToString
+public class AddImgFormBean {
+
+    private Integer id;
+
+    private Integer boulderProblemId;
+
+    private Integer userId;
+
+    private List<MultipartFile> images = new ArrayList<>();
+
+    private String imageUrl;
+
+    private String description;
+
 }
