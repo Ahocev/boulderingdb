6b7b4a3526fbb713c276a431cd45438d0464c9e8
Ahocev <Ahocev@gmail.com>
2024-08-20 09:57:58 -0700

    added findByBoulderProblemId to AdditionalImageDAO so jsp can access list of additional images by BPID

--- src/main/java/info/alexhocevarsmith/boulderingdb/database/dao/AdditionalImageDAO.java
+++ src/main/java/info/alexhocevarsmith/boulderingdb/database/dao/AdditionalImageDAO.java
@@ -3,10 +3,14 @@ package info.alexhocevarsmith.boulderingdb.database.dao;
 import info.alexhocevarsmith.boulderingdb.database.entity.AdditionalImage;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AdditionalImageDAO extends JpaRepository<AdditionalImage, Long> {
 
     AdditionalImage findById(Integer id);
 
     AdditionalImage findByImageUrl(String imageUrl);
+
+    List<AdditionalImage> findByBoulderProblemId(Integer boulderProblemId);
 
 }
